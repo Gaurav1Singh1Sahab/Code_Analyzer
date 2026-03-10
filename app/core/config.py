6087464d77ecfeb352e9fe5ff74d98e8d5795b86
Gaurav1Singh1Sahab <gs94gauravsingh@gmail.com>
2026-03-10 21:51:46 +0530

    added environment config system

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings():
+    APP_NAME: str = os.getenv("APP_NAME")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    UPLOAD_DIR: str = os.getenv("UPLOAD_DIR")
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+from app.core.config import settings
+
 
 app = FastAPI(
-    title="Code Analyzer AI",
-    description="A code analyzer AI that analyzes code and provides insights",
+    title=settings.APP_NAME,
+    description=f"{settings.APP_NAME} is a code analyzer AI that analyzes code and provides insights",
     version="1.0.0",
     
 )
 
 @app.get("/")
 def root():
-    return {"message": "Code Analyzer API is running"}
+    return {"message": f"{settings.APP_NAME} is running"}
