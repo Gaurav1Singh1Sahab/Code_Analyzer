83f8357c147c0981a6607a174525c8e38af90262
Gaurav1Singh1Sahab <gs94gauravsingh@gmail.com>
2026-03-11 15:12:28 +0530

    add user model and create users table

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
 from app.core.config import settings
-from app.db.database import engine
+from app.db.database import engine, Base
 
-
+import app.db.models
 
 app = FastAPI(
     title=settings.APP_NAME,
-    description=f"{settings.APP_NAME} is a code analyzer AI that analyzes code and provides insights",
-    version="1.0.0",
-    
+    version="1.0"
 )
 
+# print(Base.metadata.tables)
 
-print(engine)
+# print(engine.url)
 
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
-    return {"message": f"{settings.APP_NAME} is running"}
+    return {"message": f"{settings.APP_NAME} Running"}
