bce5373ad551f5c1dcb6faae66f5f61e4f399b65
Gaurav1Singh1Sahab <gs94gauravsingh@gmail.com>
2026-03-13 15:24:07 +0530

    Created a new table for PROJECT

--- app/db/models.py
+++ app/db/models.py
@@ -2,6 +2,9 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from app.db.database import Base
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class User(Base):
     __tablename__ = "users"
@@ -11,3 +14,21 @@ class User(Base):
     password = Column(String, nullable=False)
     role = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String, nullable=False)
+
+    description = Column(String, nullable=True)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    owner = relationship("User")
